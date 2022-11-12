0c16bc513fbfab4b721b5bf2f291a9f3441e0138
fanolans <mailenolan@gmail.com>
2022-11-13 06:30:41 +0700

    Create bottom navigation bar

---
+++ lib/presentations/pages/home_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ACF News'),
+      ),
+      body: const Center(
+        child: Text('Screen'),
+      ),
+      bottomNavigationBar: ConvexAppBar(
+        items: const [
+          TabItem(icon: Icons.home, title: 'Home'),
+          TabItem(icon: Icons.search, title: 'Search'),
+          TabItem(icon: Icons.bookmark, title: 'Bookmark'),
+          TabItem(icon: Icons.account_box_rounded, title: 'Account'),
+        ],
+      ),
+    );
+  }
+}
