961e447e472fb3582b454ebe9127684719eec463
fanolans <mailenolan@gmail.com>
2022-11-13 06:31:39 +0700

    Create utils routes

---
+++ lib/utils/routes.dart
@@ -0,0 +1,6 @@
+class Routes {
+  static const String homePage = '/';
+  static const String searchPage = '/search';
+  static const String bookmarkPage = '/bookmark';
+  static const String accountPage = '/archieved';
+}
