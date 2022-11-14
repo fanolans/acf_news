bc1f421ce02cd8c08c5619cbb55d52a3a96727ce
fanolans <mailenolan@gmail.com>
2022-11-14 17:32:10 +0700

    Create splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:acf_news/presentations/pages/home_page.dart';
+import 'package:acf_news/presentations/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'ACF News',
-      home: HomePage(),
+      home: SplashScreen(),
     );
   }
 }
