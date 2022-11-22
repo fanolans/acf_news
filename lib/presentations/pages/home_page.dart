04d2fd0c13ce32e745a749adc0155d679788f83f
Chaerulf <chairulfauzi27@gmail.com>
2022-11-22 17:25:12 +0800

    fixing

--- lib/presentations/pages/home_page.dart
+++ lib/presentations/pages/home_page.dart
@@ -1,5 +1,8 @@
+import 'package:acf_news/data/api/api_service.dart';
 import 'package:acf_news/data/model/article_model.dart';
-import 'package:acf_news/presentations/widgets/custom_tag.dart';
+import 'package:acf_news/presentations/widgets/card_breaking_news_widget.dart';
+import 'package:acf_news/presentations/widgets/card_news_of_theday_widget.dart';
+import 'package:acf_news/presentations/widgets/customtag_newsoftheday_widget.dart';
 import 'package:acf_news/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -13,190 +16,48 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late Future<ArticlesResult> _article;
+
   @override
+  void initState() {
+    super.initState();
+    _article = ApiServices().topHeadlines();
+  }
+
   Widget build(BuildContext context) {
-    Article article = Article.articles[0];
-    return Scaffold(
-      body: SafeArea(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            Stack(
-              children: [
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.48,
-                  decoration: const BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(40),
-                      bottomRight: Radius.circular(40),
-                    ),
-                  ),
-                ),
-                Card(
-                  child: Text(
-                    'Berita hari ini',
-                    style: GlobalFunctions.textTheme(context: context)
-                        .headline3!
-                        .copyWith(color: Colors.black, fontSize: 14),
-                  ),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
+    return FutureBuilder(
+      future: _article,
+      builder: (context, AsyncSnapshot<ArticlesResult> snapshot) {
+        var state = snapshot.connectionState;
+        if (state != ConnectionState.done) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else {
+          if (snapshot.hasData) {
+            var article = snapshot.data?.articles[0];
+            return SafeArea(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  NewsOfTheDay(article: article),
+                  BreakingNews(article: article),
+                ],
+              ),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Material(
+                child: Text(snapshot.error.toString()),
+              ),
+            );
+          } else {
+            return const Material(
+              child: Text(''),
+            );
+          }
+        }
+      },
     );
   }
 }
-
-// // NEWS OF THE DAY
-// class _NewsOfTheDay extends StatelessWidget {
-//   const _NewsOfTheDay({
-//     Key? key,
-//     required this.article,
-//   }) : super(key: key);
-
-//   final Article article;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       padding: const EdgeInsets.all(20),
-//       height: MediaQuery.of(context).size.height * 0.48,
-//       decoration: BoxDecoration(
-//         image: DecorationImage(
-//             image: NetworkImage(Article.articles[0].thumbnail),
-//             fit: BoxFit.cover),
-//         borderRadius: const BorderRadius.only(
-//           bottomLeft: Radius.circular(40),
-//           bottomRight: Radius.circular(40),
-//         ),
-//       ),
-//       child: Column(
-//         mainAxisAlignment: MainAxisAlignment.end,
-//         crossAxisAlignment: CrossAxisAlignment.start,
-//         children: [
-//           CustomTag(
-//             backgroundcolor: Colors.grey.withAlpha(150),
-//             children: [
-//               Text(
-//                 "News of the Day",
-//                 style: textTheme.subtitle2?.copyWith(color: kColorSecondary),
-//               ),
-//             ],
-//           ),
-//           const SizedBox(
-//             height: 10,
-//           ),
-//           Text(
-//             article.title,
-//             style: textTheme.headline4?.copyWith(
-//               color: kColorSecondary,
-//             ),
-//           ),
-//           TextButton(
-//             style: TextButton.styleFrom(padding: EdgeInsets.zero),
-//             onPressed: () {},
-//             child: Row(
-//               children: [
-//                 Text(
-//                   "Read more",
-//                   style: textTheme.subtitle2?.copyWith(color: kColorSecondary),
-//                 ),
-//                 const SizedBox(
-//                   width: 5,
-//                 ),
-//                 const Icon(
-//                   Icons.arrow_right_alt,
-//                   color: Colors.white,
-//                   size: 20,
-//                 )
-//               ],
-//             ),
-//           )
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-// // BREAKING NEWS
-// class _BreakingNews extends StatelessWidget {
-//   const _BreakingNews({
-//     Key? key,
-//     required this.articles,
-//   }) : super(key: key);
-
-//   final List<Article> articles;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Padding(
-//       padding: const EdgeInsets.all(20),
-//       child: Column(
-//         children: [
-//           Row(
-//             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//             children: [
-//               Text(
-//                 "Breaking News",
-//                 style: textTheme.headline5?.copyWith(color: Colors.black),
-//               ),
-//               Text("More",
-//                   style: textTheme.subtitle2?.copyWith(color: Colors.black)),
-//             ],
-//           ),
-//           const SizedBox(
-//             height: 20,
-//           ),
-//           SizedBox(
-//             height: 250,
-//             child: ListView.builder(
-//               scrollDirection: Axis.horizontal,
-//               itemCount: articles.length,
-//               itemBuilder: (context, index) {
-//                 return Container(
-//                   height: 122,
-//                   width: 193,
-//                   margin: const EdgeInsets.only(right: 10),
-//                   child: Column(
-//                     children: [
-//                       Container(
-//                         height: 122,
-//                         width: 193,
-//                         decoration: BoxDecoration(
-//                           borderRadius: BorderRadius.circular(15),
-//                           image: DecorationImage(
-//                               image: NetworkImage(
-//                                   Article.articles[index].thumbnail),
-//                               fit: BoxFit.cover),
-//                         ),
-//                       ),
-//                       const SizedBox(
-//                         height: 10,
-//                       ),
-//                       Text(
-//                         articles[index].title,
-//                         maxLines: 2,
-//                         style:
-//                             textTheme.subtitle1?.copyWith(color: Colors.black),
-//                       ),
-//                       const SizedBox(
-//                         height: 5,
-//                       ),
-//                       Text(
-//                         '${DateTime.now()}',
-//                         style: Theme.of(context).textTheme.bodySmall,
-//                       ),
-//                     ],
-//                   ),
-//                 );
-//               },
-//             ),
-//           )
-//         ],
-//       ),
-//     );
-//   }
-// }
