1d6c6d4e09494a076d253f5383352f8d93e936fc
fanolans <mailenolan@gmail.com>
2022-11-27 00:46:23 +0700

    Add app bar

--- lib/presentations/pages/article_list_page.dart
+++ lib/presentations/pages/article_list_page.dart
@@ -1,3 +1,4 @@
+import 'package:acf_news/presentations/pages/bookmark_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -61,6 +62,19 @@ class _ArticleListPageState extends State<ArticleListPage> {
 
   Widget _buildAndroid(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => null,
+          icon: const Icon(Icons.menu_rounded),
+        ),
+        title: const Text('ACF News'),
+        actions: [
+          IconButton(
+            onPressed: () => const BookmarkPage(),
+            icon: const Icon(Icons.bookmarks_rounded),
+          ),
+        ],
+      ),
       body: _buildList(context),
     );
   }
