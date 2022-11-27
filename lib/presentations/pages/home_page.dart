894464c6f3f7522f9dc102ef51719e12356c530f
fanolans <mailenolan@gmail.com>
2022-11-27 18:33:50 +0700

    Remove bottom nav bar

--- lib/presentations/pages/home_page.dart
+++ lib/presentations/pages/home_page.dart
@@ -1,17 +1,10 @@
-import 'dart:io';
-
 import 'package:acf_news/presentations/pages/article_list_page.dart';
-import 'package:acf_news/presentations/pages/bookmark_page.dart';
 import 'package:acf_news/presentations/pages/search_page.dart';
-import 'package:acf_news/presentations/pages/settings_page.dart';
+import 'package:acf_news/presentations/widgets/category_card.dart';
 import 'package:acf_news/utils/styles.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import '../../data/api/api_service.dart';
-import '../../data/provider/news_provider.dart';
-import '../widgets/platform_widget.dart';
+import '../widgets/custom_drawer.dart';
 
 class HomePage extends StatefulWidget {
   static const routeName = '/home_page';
@@ -23,78 +16,61 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _bottomNavIndex = 0;
-
-  final List<Widget> _listWidget = [
-    ChangeNotifierProvider<NewsProvider>(
-      create: (_) => NewsProvider(
-        apiService: ApiServices(),
-      ),
-      child: const ArticleListPage(),
-    ),
-    const SearchPage(),
-    const BookmarkPage(),
-    const OtherPage(),
-  ];
-
-  final List<BottomNavigationBarItem> _bottomNavBarItems = [
-    BottomNavigationBarItem(
-      icon: Icon(Platform.isIOS ? CupertinoIcons.home : Icons.home),
-      label: 'Home',
-    ),
-    BottomNavigationBarItem(
-      icon: Icon(Platform.isIOS ? CupertinoIcons.search : Icons.search),
-      label: 'Search',
-    ),
-    BottomNavigationBarItem(
-      icon: Icon(Platform.isIOS ? CupertinoIcons.bookmark : Icons.bookmark),
-      label: 'Bookmark',
-    ),
-    BottomNavigationBarItem(
-      icon: Icon(Platform.isIOS ? CupertinoIcons.list_bullet : Icons.list),
-      label: 'Others',
-    ),
-  ];
-
-  void _onBottomNavTapped(int index) {
-    setState(
-      () {
-        _bottomNavIndex = index;
-      },
-    );
-  }
-
-  Widget _buildAndroid(BuildContext context) {
-    return Scaffold(
-      body: _listWidget[_bottomNavIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        unselectedItemColor: Colors.grey,
-        selectedItemColor: kColorSecondary,
-        currentIndex: _bottomNavIndex,
-        items: _bottomNavBarItems,
-        onTap: _onBottomNavTapped,
-      ),
-    );
-  }
-
-  Widget _buildIos(BuildContext context) {
-    return CupertinoTabScaffold(
-      tabBar: CupertinoTabBar(
-        items: _bottomNavBarItems,
-        inactiveColor: Colors.grey,
-        activeColor: kColorSecondary,
-      ),
-      tabBuilder: (context, index) {
-        return _listWidget[index];
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    return PlatformWidget(
-      androidBuilder: _buildAndroid,
-      iosBuilder: _buildIos,
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        drawer: const CustomDrawer(),
+        body: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                title: const Text('ACF News'),
+                actions: [
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, SearchPage.routeName);
+                    },
+                    icon: const Icon(Icons.search),
+                  )
+                ],
+                pinned: true,
+                floating: true,
+                bottom: TabBar(
+                  indicatorColor: kColorPrimary,
+                  tabs: [
+                    _buildTabBarItem(
+                        'Top Headline', Icons.new_releases_rounded),
+                    _buildTabBarItem(
+                        'Health', Icons.health_and_safety_outlined),
+                  ],
+                ),
+              ),
+            ];
+          },
+          body: const TabBarView(
+            children: [
+              ArticleListPage(),
+              ArticleListPage(),
+            ],
+          ),
+        ),
+      ),
     );
   }
+
+  Widget _buildTabBarItem(String title, IconData iconData) => Padding(
+        padding: const EdgeInsets.only(top: 4.0, bottom: 12.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(iconData),
+            const SizedBox(
+              width: 12.0,
+            ),
+            Text(title),
+          ],
+        ),
+      );
 }
