e6b95e9dbe3deca5ad391e230b1d6de7ed414d55
fanolans <mailenolan@gmail.com>
2022-11-28 19:15:02 +0700

    Create background service

---
+++ lib/utils/background_service.dart
@@ -0,0 +1,38 @@
+import 'dart:ui';
+import 'dart:isolate';
+import 'package:acf_news/main.dart';
+import 'package:acf_news/data/api/api_service.dart';
+
+import 'notification_helper.dart';
+
+final ReceivePort port = ReceivePort();
+
+class BackgroundService {
+  static BackgroundService? _instance;
+  static const String _isolateName = 'isolate';
+  static SendPort? _uiSendPort;
+
+  BackgroundService._internal() {
+    _instance = this;
+  }
+
+  factory BackgroundService() => _instance ?? BackgroundService._internal();
+
+  void initializeIsolate() {
+    IsolateNameServer.registerPortWithName(
+      port.sendPort,
+      _isolateName,
+    );
+  }
+
+  static Future<void> callback() async {
+    print('Alarm fired!');
+    final NotificationHelper notificationHelper = NotificationHelper();
+    var result = await ApiServices().topHeadlines();
+    await notificationHelper.showNotification(
+        flutterLocalNotificationsPlugin, result);
+
+    _uiSendPort ??= IsolateNameServer.lookupPortByName(_isolateName);
+    _uiSendPort?.send(null);
+  }
+}
