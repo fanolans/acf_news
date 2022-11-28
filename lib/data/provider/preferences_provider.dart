8feef67aca0aa3dd8f69692417ec0f4037dd5b65
fanolans <mailenolan@gmail.com>
2022-11-28 19:11:56 +0700

    Create preferences provider

---
+++ lib/data/provider/preferences_provider.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import '../../utils/styles.dart';
+import '../preferences/preferences_helper.dart';
+
+class PreferencesProvider extends ChangeNotifier {
+  PreferencesHelper preferencesHelper;
+
+  PreferencesProvider({required this.preferencesHelper}) {
+    _getTheme();
+    _getDailyNewsPreferences();
+  }
+
+  bool _isDarkTheme = false;
+  bool get isDarkTheme => _isDarkTheme;
+
+  bool _isDailyNewsActive = false;
+  bool get isDailyNewsActive => _isDailyNewsActive;
+
+  ThemeData get themeData => _isDarkTheme ? darkTheme : lightTheme;
+
+  void _getTheme() async {
+    _isDarkTheme = await preferencesHelper.isDarkTheme;
+    notifyListeners();
+  }
+
+  void _getDailyNewsPreferences() async {
+    _isDailyNewsActive = await preferencesHelper.isDailyNewsActive;
+    notifyListeners();
+  }
+
+  void enableDarkTheme(bool value) {
+    preferencesHelper.setDarkTheme(value);
+    _getTheme();
+  }
+
+  void enableDailyNews(bool value) {
+    preferencesHelper.setDailyNews(value);
+    _getDailyNewsPreferences();
+  }
+}
