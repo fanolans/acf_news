07329882d1b5bec3ad1a6a8b282930607a26655f
fanolans <mailenolan@gmail.com>
2022-11-28 19:12:42 +0700

    Fix bookmark page

--- lib/presentations/pages/bookmark_page.dart
+++ lib/presentations/pages/bookmark_page.dart
@@ -1,19 +1,17 @@
+import 'package:acf_news/utils/state_result.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import '../../utils/global_functions.dart';
-import '../../utils/styles.dart';
+import '../../data/provider/database_provider.dart';
+import '../widgets/card_bookmark.dart';
+import '../widgets/platform_widget.dart';
 
-class BookmarkPage extends StatefulWidget {
+class BookmarkPage extends StatelessWidget {
   const BookmarkPage({Key? key}) : super(key: key);
   static const routeName = '/bookmark';
 
-  @override
-  State<BookmarkPage> createState() => _BookmarkPageState();
-}
-
-class _BookmarkPageState extends State<BookmarkPage> {
-  @override
-  Widget build(BuildContext context) {
+  Widget _buildAndroid(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -21,11 +19,44 @@ class _BookmarkPageState extends State<BookmarkPage> {
         ),
         elevation: 0,
       ),
-      body: const SafeArea(
-        child: Center(
-          child: Text('Bookmark Page'),
-        ),
-      ),
+      body: _buildList(),
+    );
+  }
+
+  Widget _buildIos(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: _buildList(),
+    );
+  }
+
+  Widget _buildList() {
+    return Consumer<DatabaseProvider>(
+      builder: (context, provider, child) {
+        if (provider.state == StateResult.hasData) {
+          return ListView.builder(
+            itemCount: provider.bookmarks.length,
+            itemBuilder: (context, index) {
+              return CardBookmark(
+                article: provider.bookmarks[index],
+              );
+            },
+          );
+        } else {
+          return Center(
+            child: Material(
+              child: Text(provider.message),
+            ),
+          );
+        }
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PlatformWidget(
+      androidBuilder: _buildAndroid,
+      iosBuilder: _buildIos,
     );
   }
 }
