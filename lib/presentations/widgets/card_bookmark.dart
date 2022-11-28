7f8b4b9b3d8cf5b84634f9dee904a58f32cc0112
fanolans <mailenolan@gmail.com>
2022-11-28 19:14:12 +0700

    Create card bookmark widget

---
+++ lib/presentations/widgets/card_bookmark.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../data/model/article_model.dart';
+import '../../data/provider/database_provider.dart';
+import '../pages/article_detail_page.dart';
+
+class CardBookmark extends StatelessWidget {
+  final Article article;
+
+  const CardBookmark({Key? key, required this.article}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<DatabaseProvider>(
+      builder: (context, provider, child) {
+        return FutureBuilder<bool>(
+          future: provider.isBookmarked(article.url),
+          builder: (context, snapshot) {
+            var isBookmarked = snapshot.data ?? false;
+            return Material(
+              child: ListTile(
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                leading: Hero(
+                  tag: article.urlToImage!,
+                  child: Image.network(
+                    article.urlToImage!,
+                    width: 100,
+                  ),
+                ),
+                title: Text(
+                  article.title,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                subtitle: Text(article.author ?? ""),
+                trailing: isBookmarked
+                    ? IconButton(
+                        icon: const Icon(Icons.bookmark),
+                        color: Theme.of(context).colorScheme.secondary,
+                        onPressed: () => provider.removeBookmark(article.url),
+                      )
+                    : IconButton(
+                        icon: const Icon(Icons.bookmark_border),
+                        color: Theme.of(context).colorScheme.secondary,
+                        onPressed: () => provider.addBookmark(article),
+                      ),
+                onTap: () => Navigator.pushNamed(
+                  context,
+                  ArticleDetailPage.routeName,
+                  arguments: article,
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
