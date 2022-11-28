7021b59875d21d34f861d333af95f95ba1440793
fanolans <mailenolan@gmail.com>
2022-11-28 19:13:57 +0700

    Add database provider

--- lib/presentations/widgets/card_article_widget.dart
+++ lib/presentations/widgets/card_article_widget.dart
@@ -1,7 +1,9 @@
 import 'package:acf_news/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../data/model/article_model.dart';
+import '../../data/provider/database_provider.dart';
 import '../pages/article_detail_page.dart';
 
 class CardArticle extends StatelessWidget {
@@ -11,112 +13,142 @@ class CardArticle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        InkWell(
-          onTap: () => Navigator.pushNamed(
-            context,
-            ArticleDetailPage.routeName,
-            arguments: article,
-          ),
-          child: Container(
-            height: MediaQuery.of(context).size.height * 0.40,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(article.urlToImage!),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Padding(
-          padding: const EdgeInsets.only(
-            left: 16,
-            right: 16,
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const Icon(Icons.account_circle_rounded),
-                  const SizedBox(
-                    width: 5,
+    return Consumer<DatabaseProvider>(
+      builder: (context, provider, child) {
+        return FutureBuilder<bool>(
+          future: provider.isBookmarked(article.url),
+          builder: (context, snapshot) {
+            var isBookmarked = snapshot.data ?? false;
+            return Column(
+              children: [
+                InkWell(
+                  onTap: () => Navigator.pushNamed(
+                    context,
+                    ArticleDetailPage.routeName,
+                    arguments: article,
                   ),
-                  Text(
-                    article.author!,
-                    style: textTheme.bodySmall?.copyWith(
-                      color: Colors.black.withAlpha(150),
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.40,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(article.urlToImage!),
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: const [
-                  Icon(Icons.share),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Icon(Icons.bookmark_border_outlined),
-                ],
-              ),
-            ],
-          ),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 15, right: 15),
-          child: InkWell(
-            onTap: () => Navigator.pushNamed(
-              context,
-              ArticleDetailPage.routeName,
-              arguments: article,
-            ),
-            child: Column(
-              children: [
-                Text(
-                  article.title,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: textTheme.subtitle1?.copyWith(
-                      color: Colors.black, fontWeight: FontWeight.bold),
                 ),
                 const SizedBox(
-                  height: 5,
+                  height: 10,
                 ),
-                Text(
-                  article.description!,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: textTheme.subtitle1?.copyWith(
-                      color: Colors.black.withAlpha(150), fontSize: 15),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    '${DateTime.now().difference(article.publishedAt!).inHours} hours ago',
-                    style: textTheme.bodySmall?.copyWith(color: Colors.grey),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 5,
+                    right: 5,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.account_circle_rounded),
+                            color: Theme.of(context).colorScheme.secondary,
+                            onPressed: () => '',
+                          ),
+                          Text(
+                            article.author!,
+                            style: textTheme.bodySmall?.copyWith(
+                              color: Colors.black.withAlpha(150),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.share),
+                            color: Theme.of(context).colorScheme.secondary,
+                            onPressed: () => '',
+                          ),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          isBookmarked
+                              ? IconButton(
+                                  icon: const Icon(Icons.bookmark),
+                                  color:
+                                      Theme.of(context).colorScheme.secondary,
+                                  onPressed: () =>
+                                      provider.removeBookmark(article.url),
+                                )
+                              : IconButton(
+                                  icon: const Icon(Icons.bookmark_border),
+                                  color:
+                                      Theme.of(context).colorScheme.secondary,
+                                  onPressed: () =>
+                                      provider.addBookmark(article),
+                                ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15, right: 15),
+                  child: InkWell(
+                    onTap: () => Navigator.pushNamed(
+                      context,
+                      ArticleDetailPage.routeName,
+                      arguments: article,
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          article.title,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: textTheme.subtitle1?.copyWith(
+                              color: Colors.black, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          article.description!,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: textTheme.subtitle1?.copyWith(
+                              color: Colors.black.withAlpha(150), fontSize: 15),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            '${DateTime.now().difference(article.publishedAt!).inHours} hours ago',
+                            style: textTheme.bodySmall
+                                ?.copyWith(color: Colors.grey),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                )
               ],
-            ),
-          ),
-        ),
-        const SizedBox(
-          height: 20,
-        )
-      ],
+            );
+          },
+        );
+      },
     );
   }
 }
