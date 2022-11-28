53a516542646e20bb03f448cf5eaccff09bfdfe7
fanolans <mailenolan@gmail.com>
2022-11-28 19:10:33 +0700

    Fix main

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,112 @@
+import 'dart:io';
+
+import 'package:acf_news/data/api/api_service.dart';
 import 'package:acf_news/presentations/pages/article_detail_page.dart';
+import 'package:acf_news/presentations/pages/article_list_page.dart';
 import 'package:acf_news/presentations/pages/article_web_view.dart';
 import 'package:acf_news/presentations/pages/bookmark_page.dart';
 import 'package:acf_news/presentations/pages/home_page.dart';
-import 'package:acf_news/presentations/pages/other_page.dart';
 import 'package:acf_news/presentations/pages/search_page.dart';
-import 'package:acf_news/utils/styles.dart';
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+import 'data/database/database_helper.dart';
 import 'data/model/article_model.dart';
+import 'data/preferences/preferences_helper.dart';
+import 'data/provider/database_provider.dart';
+import 'data/provider/news_provider.dart';
+import 'data/provider/preferences_provider.dart';
+import 'data/provider/scheduling_provider.dart';
+import 'utils/background_service.dart';
+import 'utils/navigation.dart';
+import 'utils/notification_helper.dart';
 
-void main() => runApp(const MyApp());
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final NotificationHelper notificationHelper = NotificationHelper();
+  final BackgroundService service = BackgroundService();
+
+  service.initializeIsolate();
+
+  if (Platform.isAndroid) {
+    await AndroidAlarmManager.initialize();
+  }
+  await notificationHelper.initNotifications(flutterLocalNotificationsPlugin);
+
+  runApp(
+    const MyApp(),
+  );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'ACF News',
-      theme: ThemeData(
-        colorScheme: Theme.of(context).colorScheme.copyWith(
-              primary: kColorPrimary,
-              onPrimary: kColorSecondary,
-              secondary: kColorSecondary,
-            ),
-        scaffoldBackgroundColor: Colors.white,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        textTheme: textTheme,
-        fontFamily: 'Inter',
-        appBarTheme: const AppBarTheme(elevation: 0),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            foregroundColor: Colors.white,
-            backgroundColor: kColorSecondary,
-            textStyle: const TextStyle(),
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(0),
-              ),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => NewsProvider(
+            apiService: ApiServices(),
+          ),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => SchedulingProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => PreferencesProvider(
+            preferencesHelper: PreferencesHelper(
+              sharedPreferences: SharedPreferences.getInstance(),
             ),
           ),
         ),
+        ChangeNotifierProvider(
+          create: (_) => DatabaseProvider(
+            databaseHelper: DatabaseHelper(),
+          ),
+        ),
+      ],
+      child: Consumer<PreferencesProvider>(
+        builder: (context, provider, child) {
+          return MaterialApp(
+            title: 'News App',
+            theme: provider.themeData,
+            builder: (context, child) {
+              return CupertinoTheme(
+                data: CupertinoThemeData(
+                  brightness:
+                      provider.isDarkTheme ? Brightness.dark : Brightness.light,
+                ),
+                child: Material(
+                  child: child,
+                ),
+              );
+            },
+            navigatorKey: navigatorKey,
+            initialRoute: HomePage.routeName,
+            routes: {
+              HomePage.routeName: (context) => const HomePage(),
+              SearchPage.routeName: (context) => const SearchPage(),
+              BookmarkPage.routeName: (context) => const BookmarkPage(),
+              ArticleDetailPage.routeName: (context) => ArticleDetailPage(
+                    article:
+                        ModalRoute.of(context)?.settings.arguments as Article,
+                  ),
+              ArticleWebView.routeName: (context) => ArticleWebView(
+                    url: ModalRoute.of(context)?.settings.arguments as String,
+                  ),
+            },
+          );
+        },
       ),
-      initialRoute: HomePage.routeName,
-      routes: {
-        HomePage.routeName: (context) => const HomePage(),
-        ArticleDetailPage.routeName: (context) => ArticleDetailPage(
-              article: ModalRoute.of(context)?.settings.arguments as Article,
-            ),
-        ArticleWebView.routeName: (context) => ArticleWebView(
-              url: ModalRoute.of(context)?.settings.arguments as String,
-            ),
-        SearchPage.routeName: (context) => const SearchPage(),
-        BookmarkPage.routeName: (context) => const BookmarkPage(),
-        OtherPage.routeName: (context) => const OtherPage(),
-      },
     );
   }
 }
