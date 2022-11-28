86e420af970e64a13e3de7996a98ff8327e2127a
fanolans <mailenolan@gmail.com>
2022-11-28 19:11:03 +0700

    Fix model

--- lib/data/model/article_model.dart
+++ lib/data/model/article_model.dart
@@ -12,15 +12,26 @@ class ArticlesResult {
   factory ArticlesResult.fromJson(Map<String, dynamic> json) => ArticlesResult(
         status: json["status"],
         totalResults: json["totalResults"],
-        articles: List<Article>.from((json["articles"] as List)
-            .map((x) => Article.fromJson(x))
-            .where((article) =>
-                article.author != null &&
-                article.description != null &&
-                article.urlToImage != null &&
-                article.publishedAt != null &&
-                article.content != null)),
+        articles: List<Article>.from(
+          (json["articles"] as List).map((x) => Article.fromJson(x)).where(
+              (article) =>
+                  article.author != null &&
+                  article.description != null &&
+                  article.urlToImage != null &&
+                  article.publishedAt != null &&
+                  article.content != null),
+        ),
       );
+
+  Map<String, dynamic> toJson() => {
+        "status": status,
+        "totalResults": totalResults,
+        "articles": List<dynamic>.from(
+          articles.map(
+            (x) => x.toJson(),
+          ),
+        ),
+      };
 }
 
 class Article {
@@ -45,10 +56,20 @@ class Article {
   factory Article.fromJson(Map<String, dynamic> json) => Article(
         author: json["author"],
         title: json["title"],
-        description: json["description"],
+        description: json["description"] ?? "-",
         url: json["url"],
         urlToImage: json["urlToImage"],
         publishedAt: DateTime.parse(json["publishedAt"]),
         content: json["content"],
       );
+
+  Map<String, dynamic> toJson() => {
+        "author": author,
+        "title": title,
+        "description": description,
+        "url": url,
+        "urlToImage": urlToImage,
+        "publishedAt": publishedAt?.toIso8601String(),
+        "content": content,
+      };
 }
