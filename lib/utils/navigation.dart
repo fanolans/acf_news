61b7901c59c336b0764f41a91af6adadfff2575d
fanolans <mailenolan@gmail.com>
2022-11-28 19:15:42 +0700

    Add back navigation

--- lib/utils/navigation.dart
+++ lib/utils/navigation.dart
@@ -1,9 +1,12 @@
+import 'package:acf_news/data/model/article_model.dart';
 import 'package:flutter/material.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 class Navigation {
-  static intentWithData(String routeName) {
+  static intentWithData(String routeName, Article article) {
     navigatorKey.currentState?.pushNamed(routeName);
   }
+
+  static back() => navigatorKey.currentState?.pop();
 }
