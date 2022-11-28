b45cb49c13355f2d19ee9db1fded991825d3805f
fanolans <mailenolan@gmail.com>
2022-11-28 19:10:50 +0700

    Create database helper

---
+++ lib/data/database/database_helper.dart
@@ -0,0 +1,82 @@
+import 'package:sqflite/sqflite.dart';
+
+import '../model/article_model.dart';
+
+class DatabaseHelper {
+  static DatabaseHelper? _instance;
+  static Database? _database;
+
+  DatabaseHelper._internal() {
+    _instance = this;
+  }
+
+  factory DatabaseHelper() => _instance ?? DatabaseHelper._internal();
+
+  static const String _tblBookmark = 'bookmarks';
+
+  Future<Database> _initializeDb() async {
+    var path = await getDatabasesPath();
+    var db = openDatabase(
+      '$path/newsapp.db',
+      onCreate: (db, version) async {
+        await db.execute('''CREATE TABLE $_tblBookmark (
+             url TEXT PRIMARY KEY,
+             author TEXT,
+             title TEXT,
+             description TEXT,
+             urlToImage TEXT,
+             publishedAt TEXT,
+             content TEXT
+           )     
+        ''');
+      },
+      version: 1,
+    );
+
+    return db;
+  }
+
+  Future<Database?> get database async {
+    _database ??= await _initializeDb();
+
+    return _database;
+  }
+
+  Future<void> insertBookmark(Article article) async {
+    final db = await database;
+    await db!.insert(_tblBookmark, article.toJson());
+  }
+
+  Future<List<Article>> getBookmarks() async {
+    final db = await database;
+    List<Map<String, dynamic>> results = await db!.query(_tblBookmark);
+
+    return results.map((res) => Article.fromJson(res)).toList();
+  }
+
+  Future<Map> getBookmarkByUrl(String url) async {
+    final db = await database;
+
+    List<Map<String, dynamic>> results = await db!.query(
+      _tblBookmark,
+      where: 'url = ?',
+      whereArgs: [url],
+    );
+
+    if (results.isNotEmpty) {
+      return results.first;
+    } else {
+      return {};
+    }
+  }
+
+  Future<void> removeBookmark(String url) async {
+    final db = await database;
+
+    await db!.delete(
+      _tblBookmark,
+      where: 'url = ?',
+      whereArgs: [url],
+    );
+  }
+}
