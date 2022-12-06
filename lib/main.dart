a0aceda344984ce80d58ab894d08aa600050dc0f
fanolans <mailenolan@gmail.com>
2022-12-06 23:15:36 +0700

    Add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,25 @@
 import 'dart:io';
 
 import 'package:acf_news/data/api/api_service.dart';
+import 'package:acf_news/presentations/pages/account_page.dart';
 import 'package:acf_news/presentations/pages/article_detail_page.dart';
 import 'package:acf_news/presentations/pages/article_web_view.dart';
 import 'package:acf_news/presentations/pages/bookmark_page.dart';
 import 'package:acf_news/presentations/pages/home_page.dart';
+import 'package:acf_news/presentations/pages/other_page.dart';
+import 'package:acf_news/presentations/pages/register_page.dart';
 import 'package:acf_news/presentations/pages/search_page.dart';
-import 'package:acf_news/presentations/pages/sign_in_page.dart';
+import 'package:acf_news/presentations/pages/login_page.dart';
 import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'data/api/firebase_services.dart';
 import 'data/database/database_helper.dart';
 import 'data/model/article_model.dart';
 import 'data/preferences/preferences_helper.dart';
@@ -40,6 +46,7 @@ Future<void> main() async {
   if (Platform.isAndroid) {
     await AndroidAlarmManager.initialize();
   }
+  await Firebase.initializeApp();
   await notificationHelper.initNotifications(flutterLocalNotificationsPlugin);
 
   runApp(
@@ -80,40 +87,49 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ],
-      child: Consumer<PreferencesProvider>(
-        builder: (context, provider, child) {
-          return MaterialApp(
-            title: 'News App',
-            theme: provider.themeData,
-            builder: (context, child) {
-              return CupertinoTheme(
-                data: CupertinoThemeData(
-                  brightness:
-                      provider.isDarkTheme ? Brightness.dark : Brightness.light,
-                ),
-                child: Material(
-                  child: child,
-                ),
-              );
-            },
-            navigatorKey: navigatorKey,
-            initialRoute: SignInPage.routeName,
-            routes: {
-              HomePage.routeName: (context) => const HomePage(),
-              SearchPage.routeName: (context) => const SearchPage(),
-              BookmarkPage.routeName: (context) => const BookmarkPage(),
-              ArticleDetailPage.routeName: (context) => ArticleDetailPage(
-                    article:
-                        ModalRoute.of(context)?.settings.arguments as Article,
-                  ),
-              ArticleWebView.routeName: (context) => ArticleWebView(
-                    url: ModalRoute.of(context)?.settings.arguments as String,
-                  ),
-              SignInPage.routeName: (context) => const SignInPage(),
-            },
-          );
-        },
-      ),
+      child: StreamBuilder<User?>(
+          stream: auth.authStateChanges(),
+          builder: (context, snapshot) {
+            return Consumer<PreferencesProvider>(
+              builder: (context, provider, child) {
+                return MaterialApp(
+                  title: 'News App',
+                  theme: provider.themeData,
+                  builder: (context, child) {
+                    return CupertinoTheme(
+                      data: CupertinoThemeData(
+                        brightness: provider.isDarkTheme
+                            ? Brightness.dark
+                            : Brightness.light,
+                      ),
+                      child: Material(
+                        child: child,
+                      ),
+                    );
+                  },
+                  navigatorKey: navigatorKey,
+                  initialRoute: LoginPage.routeName,
+                  routes: {
+                    LoginPage.routeName: (context) => const LoginPage(),
+                    RegisterPage.routeName: (context) => const RegisterPage(),
+                    HomePage.routeName: (context) => const HomePage(),
+                    SearchPage.routeName: (context) => const SearchPage(),
+                    AccountPage.routeName: (context) => const AccountPage(),
+                    OtherPage.routeName: (context) => const OtherPage(),
+                    BookmarkPage.routeName: (context) => const BookmarkPage(),
+                    ArticleDetailPage.routeName: (context) => ArticleDetailPage(
+                          article: ModalRoute.of(context)?.settings.arguments
+                              as Article,
+                        ),
+                    ArticleWebView.routeName: (context) => ArticleWebView(
+                          url: ModalRoute.of(context)?.settings.arguments
+                              as String,
+                        ),
+                  },
+                );
+              },
+            );
+          }),
     );
   }
 }
