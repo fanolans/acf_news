4618156da94c7db94d78f2f37ff53e9a56df4ae0
Chaerulf <chairulfauzi27@gmail.com>
2022-12-08 00:36:12 +0700

    fixing scroll

--- lib/presentations/pages/home_page.dart
+++ lib/presentations/pages/home_page.dart
@@ -1,7 +1,14 @@
 import 'package:acf_news/presentations/pages/article_list_page.dart';
+import 'package:acf_news/presentations/pages/business_page.dart';
+import 'package:acf_news/presentations/pages/entertaiment_page.dart';
+import 'package:acf_news/presentations/pages/health_page.dart';
+import 'package:acf_news/presentations/pages/science_page.dart';
 import 'package:acf_news/presentations/pages/search_page.dart';
+import 'package:acf_news/presentations/pages/sport_page.dart';
+import 'package:acf_news/presentations/pages/technology_page.dart';
 import 'package:acf_news/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 import '../widgets/custom_drawer.dart';
 import 'category_news_page.dart';
@@ -15,109 +22,181 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(initialIndex: 0, length: 6, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
-    List<String> tabs = [
-      'Business',
-      'Entertaiment',
-      'Health',
-      'Science',
-      'Sports',
-      'Technology'
-    ];
     return Scaffold(
       drawer: const CustomDrawer(),
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            pinned: true,
-            floating: false,
-            actions: [
-              IconButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, SearchPage.routeName);
+      body: SafeArea(
+        child: Stack(
+          children: [
+            DefaultTabController(
+              length: 3,
+              child: NestedScrollView(
+                headerSliverBuilder: (context, value) {
+                  return [
+                    SliverAppBar(
+                      pinned: true,
+                      floating: false,
+                      actions: [
+                        IconButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, SearchPage.routeName);
+                          },
+                          icon: const Icon(Icons.search),
+                        )
+                      ],
+                    ),
+                    SliverList(
+                      delegate: SliverChildListDelegate(
+                        [
+                          DefaultTabController(
+                            length: 6,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 15, right: 15),
+                                  child: Text(
+                                    'Breaking News',
+                                    style: textTheme.headline5?.copyWith(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .secondary,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Container(
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.55,
+                                  child: const ArticleListPage(),
+                                ),
+                                // CategoryNews(tabs: tabs),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    SliverPersistentHeader(
+                      pinned: true,
+                      delegate: _SliverCategoryDelegate(
+                          minHeight: 50,
+                          maxHeight: 50,
+                          child: Container(
+                            color: Colors.white,
+                            child: TabBar(
+                              isScrollable: true,
+                              labelColor:
+                                  Theme.of(context).colorScheme.onPrimary,
+                              unselectedLabelColor: Colors.grey,
+                              indicatorColor: kColorNavy,
+                              controller: _tabController,
+                              tabs: [
+                                Tab(
+                                  child: Text(
+                                    "Business",
+                                    style: textTheme.headline6
+                                        ?.copyWith(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Tab(
+                                  child: Text(
+                                    "Entertaiment",
+                                    style: textTheme.headline6
+                                        ?.copyWith(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Tab(
+                                  child: Text(
+                                    "Health",
+                                    style: textTheme.headline6
+                                        ?.copyWith(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Tab(
+                                  child: Text(
+                                    "Science",
+                                    style: textTheme.headline6
+                                        ?.copyWith(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Tab(
+                                  child: Text(
+                                    "Sport",
+                                    style: textTheme.headline6
+                                        ?.copyWith(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Tab(
+                                  child: Text(
+                                    "Technology",
+                                    style: textTheme.headline6
+                                        ?.copyWith(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )),
+                    ),
+                  ];
                 },
-                icon: const Icon(Icons.search),
-              )
-            ],
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate(
-              [
-                DefaultTabController(
-                  length: tabs.length,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 15, right: 15),
-                        child: Text(
-                          'Breaking News',
-                          style: textTheme.headline5?.copyWith(
-                              color: Theme.of(context).colorScheme.secondary,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        height: MediaQuery.of(context).size.height * 0.55,
-                        child: const ArticleListPage(),
-                      ),
-                      CategoryNews(tabs: tabs),
-                    ],
-                  ),
-                )
-              ],
+                body: TabBarView(
+                  controller: _tabController,
+                  children: const [
+                    BusinessPage(),
+                    EntertaimentPage(),
+                    HealthPage(),
+                    SciencePage(),
+                    SportPage(),
+                    TechnologyPage()
+                  ],
+                ),
+              ),
             ),
-          ),
-          // SliverPersistentHeader(
-          //   pinned: true,
-          //   delegate: _HeaderSliver(),
-          // ),
-        ],
+          ],
+        ),
       ),
     );
   }
 }
 
-const _maxHeaderExtent = 100.0;
+class _SliverCategoryDelegate extends SliverPersistentHeaderDelegate {
+  _SliverCategoryDelegate(
+      {required this.minHeight, required this.maxHeight, required this.child});
+  final double minHeight;
+  final double maxHeight;
+  final Widget child;
+  @override
+  double get maxExtent => maxHeight;
 
-class _HeaderSliver extends SliverPersistentHeaderDelegate {
+  @override
+  double get minExtent => math.max(maxHeight, minHeight);
+
   @override
   Widget build(
       BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Stack(
-      children: [
-        Positioned(
-          bottom: 0,
-          left: 0,
-          right: 0,
-          child: Container(
-            height: _maxHeaderExtent,
-            color: Colors.blue,
-            child: Column(
-              children: [],
-            ),
-          ),
-        ),
-      ],
-    );
+    return SizedBox.expand(child: child);
   }
 
   @override
-  // TODO: implement maxExtent
-  double get maxExtent => _maxHeaderExtent;
-
-  @override
-  // TODO: implement minExtent
-  double get minExtent => _maxHeaderExtent;
-
-  @override
-  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
-      false;
+  bool shouldRebuild(_SliverCategoryDelegate oldDelegate) {
+    return maxHeight != oldDelegate.maxHeight ||
+        minHeight != oldDelegate.minHeight ||
+        child != oldDelegate.child;
+  }
 }
--- lib/presentations/pages/science_page.dart
+++ lib/presentations/pages/science_page.dart
@@ -23,45 +23,47 @@ class _SciencePageState extends State<SciencePage> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _article,
-      builder: (context, AsyncSnapshot<ArticlesResult> snapshot) {
-        var state = snapshot.connectionState;
-        if (state != ConnectionState.done) {
-          return Center(
-            child: CircularProgressIndicator(
-              color: Theme.of(context).colorScheme.secondary,
-            ),
-          );
-        } else {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              primary: false,
-              shrinkWrap: true,
-              itemCount: snapshot.data?.articles.length,
-              itemBuilder: (context, index) {
-                var article = snapshot.data?.articles[index];
-                return Padding(
-                  padding: const EdgeInsets.only(left: 15),
-                  child: CategoryNewsCard(
-                    article: article!,
-                  ),
-                );
-              },
-            );
-          } else if (snapshot.hasError) {
-            return const Center(
-              child: Material(
-                child: ApplicationError(),
+    return SingleChildScrollView(
+      child: FutureBuilder(
+        future: _article,
+        builder: (context, AsyncSnapshot<ArticlesResult> snapshot) {
+          var state = snapshot.connectionState;
+          if (state != ConnectionState.done) {
+            return Center(
+              child: CircularProgressIndicator(
+                color: Theme.of(context).colorScheme.secondary,
               ),
             );
           } else {
-            return const Material(
-              child: Text(''),
-            );
+            if (snapshot.hasData) {
+              return ListView.builder(
+                primary: false,
+                shrinkWrap: true,
+                itemCount: snapshot.data?.articles.length,
+                itemBuilder: (context, index) {
+                  var article = snapshot.data?.articles[index];
+                  return Padding(
+                    padding: const EdgeInsets.only(left: 15),
+                    child: CategoryNewsCard(
+                      article: article!,
+                    ),
+                  );
+                },
+              );
+            } else if (snapshot.hasError) {
+              return const Center(
+                child: Material(
+                  child: ApplicationError(),
+                ),
+              );
+            } else {
+              return const Material(
+                child: Text(''),
+              );
+            }
           }
-        }
-      },
+        },
+      ),
     );
   }
 }
